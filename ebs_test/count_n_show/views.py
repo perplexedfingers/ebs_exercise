0ac94a5941ee65fbd086ab7993bce6d73fc65635
Ray Zhu <14918698+perplexedfingers@users.noreply.github.com>
2022-03-11 08:00:16 +0000

    Use CloudFront to deliever picture

--- ebs_test/count_n_show/views.py
+++ ebs_test/count_n_show/views.py
@@ -1,5 +1,13 @@
+import random
+from urllib.parse import urljoin
+
+from django.conf import settings
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello")
+    cdn_url = settings.CDN_URL
+    chosen_one = f"{random.choice(range(1, 6))}.jpg"
+    url = urljoin(cdn_url, chosen_one)
+    return render(request, "show/template.html", context={"url": url})
